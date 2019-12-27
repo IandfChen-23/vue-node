162e2f3a7a7d8b5d2e440f6dfa3d7584ea8b2502
chenlei@npnets.com <chenlei@npnets.com>
2019-12-27 15:58:38 +0800

    注册验证

--- service/index.js
+++ service/index.js
@@ -7,38 +7,16 @@ var Koa = require('koa');
 var cors = require('koa2-cors');
 const app=new koa()
 app.use(cors());
+app.use(bodyParser());
+(async ()=>{
+    await connect()
+    initSchema()
+})();
 let user=require('./appApi/user');
 let router=new Router()
 router.use('/user',user.routes())
 app.use(router.routes())
-app.use(bodyParser())
 app.use(router.allowedMethods())
 app.listen(3000,()=>{
     console.log('service listinging at 3000');
 })
-// (async ()=>{
-//     await connect()
-//     initSchema()
-//     const User=mongoose.model('user')
-//     let oneUser= new User({
-//         userName:'leige',
-//         password:'123456'
-//     })
-//     //插入数据
-//     oneUser.save().then(()=>{
-//         console.log('插入成功');
-//     }).catch((err)=>{
-//         console.log(err);
-//     })
-//     //查找数据
-//     User.find({userName:'leige'}).then(res=>{
-//         console.log('查询成功',res)
-//     })
-//     //删除数据
-// //     User.remove({userName:'leige'}).then((res)=>{
-// //         console.log('删除成功',res)
-// //    })
-// })()
-// app.use(async (ctx)=>{
-//     ctx.body=''
-// })
--- src/components/pages/register.vue
+++ src/components/pages/register.vue
@@ -6,19 +6,23 @@
       <van-field type="password" v-model="password" label="用户名" placeholder="请输入用密码" required></van-field>
     </div>
     <div class="register-button">
-      <van-button type="primary" size="large" @click="register">马上注册</van-button>
+      <van-button type="primary" size="large" @click="register" :loading='loading'>马上注册</van-button>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Vue from 'vue';
+import { Toast } from 'vant';
+Vue.use(Toast);
 export default {
   name: "register",
   data() {
     return {
       username: "",
-      password: ""
+      password: "",
+      loading:false,
     };
   },
   methods: {
@@ -26,17 +30,35 @@ export default {
       this.$router.push("/");
     },
     register() {
-      axios.post({
-          url: "http://localhost:3000/user/register",
-          methods: "post",
-          data: {
-            username: this.username,
+        if(this.username == ''){
+          this.$toast("用户名不能为空");
+          return false;
+        }
+        if(this.password == ''){
+          this.$toast("密码不能为空");
+          return false;
+        }
+      this.loading=true;
+      const that=this;
+      axios.post("http://localhost:3000/user/register",{
+            userName: this.username,
             password: this.password
           }
-        }).then(res => {
+        ).then(res => {
           console.log(res);
+          if(res.data.code===200){
+              Toast.success(res.data.message)
+              setTimeout(function(){
+                that.$router.push('/')
+              },500)
+              
+          }else{
+              this.loading=false;
+              Toast.fail('注册失败')
+          }
         }).catch(err => {
-          console.log(err);
+          this.loading=false;
+          Toast.fail('注册失败')
         });
     }
   }
